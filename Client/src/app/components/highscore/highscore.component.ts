4c581d7c619422ebb6f56a210dc68ebdcfc8d10d
Djim <oomesdjim@gmail.com>
2018-12-10 16:37:21 +0100

    Improved highscore list with owner redirect

--- Client/src/app/components/highscore/highscore.component.ts
+++ Client/src/app/components/highscore/highscore.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChampionService } from '../../services/champion.service';
 import { Champion } from '../../models/champion';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-highscore',
@@ -11,7 +12,8 @@ export class HighscoreComponent implements OnInit {
 
   champions: Champion [];
 
-  constructor(private champService: ChampionService) { }
+  constructor(private champService: ChampionService,
+    private router: Router) { }
 
   ngOnInit() {
     this.getAllChampions()
@@ -20,6 +22,10 @@ export class HighscoreComponent implements OnInit {
   getAllChampions(): void{
     this.champService.getAll()
     .subscribe(champions =>this.champions = champions);
+  }
+
+  getOwner(id:string){
+    this.router.navigate(['/players/'+id])
   }
 
 }
